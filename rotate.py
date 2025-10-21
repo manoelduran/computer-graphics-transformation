2b5592d7632505b5e903459c85b1129957098fcf
Manoel Cendon Duran <manoel.duran@hotmail.com>
2025-10-20 22:57:46 -0300

    fix: imports

--- rotate.py
+++ rotate.py
@@ -1,10 +1,10 @@
 import cv2
-from utils import load_image_from_web, show_image
+from utils import load_image, show_image
 
 print("--- Running Exercise 3: Rotate ---")
 
 image_url = "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-original = load_image_from_web(image_url)
+original = load_image(image_url)
 
 if original is not None:
     show_image("Original", original)
